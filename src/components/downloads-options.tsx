62a5f34aae6b982ab777afdd85383acd171e13a4
GuoFei <guofei0723@gmail.com>
2020-10-15 11:57:46 +0800

    下载量图表默认按七天汇总数据

--- src/components/downloads-options.tsx
+++ src/components/downloads-options.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
-import { DatePicker, Divider, Space, Switch } from 'antd'
+import { DatePicker, Divider, Radio, Space, Switch } from 'antd'
 import moment from 'moment'
 
 export type Range = [moment.Moment | null, moment.Moment | null]
+export type TimeType = 'daily' | 'weekly'
+
 interface OptionsProps {
   range: Range | null
   onRangeChange: (range: Range | null, rangeStr: [string, string]) => void
+
+  timeType: TimeType
+  onTimeTypeChange: (timeType: TimeType) => void
 
   /**
    * 是否排除周末
@@ -17,12 +22,21 @@ interface OptionsProps {
 export default ({
   range,
   onRangeChange,
+  timeType,
+  onTimeTypeChange,
   excludeWeekends,
   onExcludeChange,
 }: OptionsProps) => {
   return (
     <div>
       <Space>
+        <Radio.Group value={timeType} onChange={(e) => onTimeTypeChange(e.target.value)}>
+          <Radio value='weekly'>weekly</Radio>
+          <Radio value='daily'>daily</Radio>
+        </Radio.Group>
+        <Switch size='small' checked={excludeWeekends} onChange={onExcludeChange} disabled={timeType === 'weekly'} />
+        <span>exclude weekends</span>
+        <Divider type='vertical' style={{ height: '1.3em'}} />
         <DatePicker.RangePicker
           ranges={{
             'last-week': () => [moment().subtract(1, 'week'), moment().subtract(1, 'day')],
@@ -37,9 +51,6 @@ export default ({
           onChange={onRangeChange}
           size='small'
         />
-        <Divider type='vertical' style={{ height: '1.3em'}} />
-        <Switch size='small' checked={excludeWeekends} onChange={onExcludeChange} />
-        <span>exclude weekends</span>
       </Space>
     </div>
   )
--- src/components/downloads.tsx
+++ src/components/downloads.tsx
@@ -4,13 +4,14 @@ import { Axis, Chart, LineAdvance } from 'bizcharts'
 import numeral from 'numeral'
 import moment from 'moment'
 import { useNpmRangeData } from '../api'
-import Options, { Range } from './downloads-options'
+import Options, { Range, TimeType } from './downloads-options'
 
 
 interface DataItem {
   downloads: number
   day: string
   lib: string
+  duration?: string
 }
 
 const lastYear = [moment().subtract(1, 'year'), moment().subtract(1, 'day')]
@@ -19,8 +20,9 @@ const lastYearStr = lastYear.map(r => r.format('YYYY-MM-DD')).join(':')
 export default ({ libs }: { libs: string[]}) => {
   const [dateRange, setDateRange] = useState<Range | null>(lastYear as Range)
   const [rangeStr, setRangeStr] = useState(lastYearStr)
+  const [timeType, setTimeType] = useState<TimeType>('weekly')
   const [excludeWeekends, setExcludeWeekends] = useState(true)
-  const { data: downloads } = useNpmRangeData(rangeStr || lastYearStr, ...libs)
+  const { data: downloads, error, isValidating } = useNpmRangeData(rangeStr || lastYearStr, ...libs)
 
   const data = useMemo(() => {
     if (!downloads) {
@@ -46,9 +48,56 @@ export default ({ libs }: { libs: string[]}) => {
     return data.filter(({ day }) => moment(day).isoWeekday() < 6)
   }, [data])
 
+  // 按周汇总的数据
+  const dataWeekly = useMemo(() => {
+    if (!downloads) {
+      return []
+    }
+
+    return libs.reduceRight<DataItem[]>((result, lib) => {
+      const libDownloads = downloads.data[lib]?.downloads || []
+      
+      let lastDay: moment.Moment | null = null
+      let item: DataItem = {
+        downloads: 0,
+        lib,
+        day: '',
+      }
+
+      for(let i = libDownloads.length - 1; i >= 0; i--) {
+        const ld = libDownloads[i]
+        const day = moment(ld.day)
+        if (!lastDay) {
+          lastDay = moment(ld.day)
+        }
+
+        const diffDay = lastDay.diff(day, 'day')
+        if (diffDay <= 6) {
+          item.downloads += ld.downloads
+        }
+        // 第七天 保存数据
+        if (diffDay === 6) {
+          item.duration = `${day.format('YYYY-MM-DD')} to ${lastDay.format('YYYY-MM-DD')}`
+          item.day = lastDay.format('YYYY-MM-DD')
+          result.unshift(item)
+
+          // 初始化下一个七天
+          lastDay = null
+          item = {
+            day: '',
+            downloads: 0,
+            lib,
+          }
+        }
+      }
+
+      return result
+    }, [])
+  }, [downloads, libs])
+
   return (
     <Card
-      title='Downloads'
+      title='NPM Downloads'
       extra={(
         <Options
           range={dateRange}
@@ -56,6 +105,8 @@ export default ({ libs }: { libs: string[]}) => {
             setDateRange(range)
             setRangeStr(range ? rangeStrs.join(':') : '')
           }}
+          timeType={timeType}
+          onTimeTypeChange={setTimeType}
           excludeWeekends={excludeWeekends}
           onExcludeChange={setExcludeWeekends}
         />
@@ -65,7 +116,7 @@ export default ({ libs }: { libs: string[]}) => {
         padding={[10, 50, 50]}
         height={300}
         autoFit
-        data={excludeWeekends ? dataWithoutWeekends : data}
+        data={timeType === 'weekly' ? dataWeekly : excludeWeekends ? dataWithoutWeekends : data}
       >
         <Axis
           name='downloads'
@@ -78,7 +129,14 @@ export default ({ libs }: { libs: string[]}) => {
           area
           position='day*downloads'
           color='lib'
-          tooltip={['lib*downloads', (name, dls) => ({ name, value: numeral(dls).format('0,0') })]}
+          tooltip={[
+            'lib*downloads*duration*day',
+            (name, dls, duration, day) => ({
+              name,
+              value: numeral(dls).format('0,0'),
+              title: duration || day
+            })
+          ]}
         />
         {downloads ? null : (
           <Empty
@@ -88,6 +146,7 @@ export default ({ libs }: { libs: string[]}) => {
               left: '50%',
               transform: 'translate(-50%, -50%)',
             }}
+            description={error || isValidating ? 'loading...' : 'No Data'}
           />
         )}
       </Chart>
