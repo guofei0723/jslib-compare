20ac91716415af6fddfb3402bfbae4b93fa66d84
GuoFei <guofei0723@gmail.com>
2020-10-19 08:08:29 +0800

    调整布局高度

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   return (
     <Router>
       <div className='App'>
-        <Layout>
+        <Layout style={{ minHeight: '100%' }}>
           <Content>
             <Switch>
               <Route path='/'>
